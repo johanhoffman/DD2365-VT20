37a336b9f4661465bb9cbf8515bd9dc50acd9a30
jonasnylund <35763296+jonasnylund@users.noreply.github.com>
2020-05-15 14:00:38 +0200

    Initial commit, half broken code

---
+++ Projects/main.py
@@ -0,0 +1,297 @@
+from fenics import *
+import mesh as msh
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+import sys
+
+
+
+def buildMesh(headway, length, aspect_ratio, angle, radius=1/10, rear=False, full=False, double=False, resolution=8):
+
+	h = 1
+	if(double):
+		h = 2
+
+	mesh, (l,h) = msh.createMesh(length, resolution*(length+headway), 
+		angle, aspect_ratio, headway, length+headway, rear=rear, full=full, 
+		double=double, radius=radius, segments=int(24*5*radius))
+	
+	x0 = headway*0.9
+	x1 = l
+	if(full):
+		x1 = l-headway*0.9
+	y1 = h
+
+	if(double):
+		y1 = h/2
+
+	if(rear):
+		x0 = 0
+		x1 = length*2
+
+	mesh = msh.refineMesh(mesh, x0, x1, 0, y1)
+	# mesh = msh.refineMesh(mesh, headway, l, 0, ys)
+
+	plt.figure()
+	plot(mesh)
+	plt.show()
+
+	return mesh, (l,h)
+
+
+
+
+T = 20.0 			# final time
+num_steps = 5000   	# number of time steps
+dt = T / num_steps 	# time step size
+mu = 10**(-2)		# dynamic viscosity
+rho = 1.4			# density
+uin = 1.0			# inflow velocity
+
+aspect_ratio = 1/10
+headway = 1
+length = 0.5
+angle = 10
+double = False
+full = False
+rear = False
+
+
+if(len(sys.argv) > 1):
+	aspect_ratio = 1/int(sys.argv[1])
+if(len(sys.argv) > 2):
+	angle = int(sys.argv[2])
+if(len(sys.argv) > 3):
+	if("double" in sys.argv[3]):
+		double = True
+if(len(sys.argv) > 4):
+	if("full" in sys.argv[4]):
+		full = True
+	if("rear" in sys.argv[4]):
+		rear = True
+
+print()
+print("Re: ", rho*uin*(1+aspect_ratio)/mu)
+print("Aspect ratio:", round(aspect_ratio, 4))
+print("Angle:", angle)
+print("Double track:", double, ", Full length:", full, "Rear view:", rear)
+
+print("Aspect ratio:", round(aspect_ratio, 4), "Angle:", angle, file=sys.stderr)
+
+if(full):
+	length = 5.0
+	# headway = 2.0
+
+if(double):
+	headway = 2.0
+
+# mesh, (xmax, ymax) = buildMesh(headway, length, aspect_ratio, angle, rear=rear,
+# 	full=full, double=double, resolution=24)
+
+mesh, (xmax, ymax) = msh.testMesh(res=32);
+print("Mesh size:", mesh.num_cells())
+
+# Define function spaces
+V = VectorFunctionSpace(mesh, "Lagrange", 2)
+Q = FunctionSpace(mesh, "Lagrange", 1)
+
+# Define boundaries
+inflowBoundary = CompiledSubDomain("on_boundary && near(x[0], 0)")
+outflowBoundary = CompiledSubDomain("on_boundary && near(x[0], "+str(xmax)+")")
+slipBoundary = CompiledSubDomain("on_boundary && ((near(x[1], 0) || near(x[1], "+str(ymax)+")) || !(near(x[0], 0) || near(x[0], "+str(xmax)+")))")
+
+# uinflow = Expression(("uin*6*x[1]*(YMAX-x[1])/(YMAX*YMAX)", "0.0"), element=V.ufl_element(), YMAX=ymax, uin=uin);
+# uinflow = Expression(("t < uin ? t : uin", "0.0"), element=V.ufl_element(), uin=uin, t=0.0)
+
+# Define Dirichlet boundary conditions
+# bcu_inflow = DirichletBC(V, uinflow, inflowBoundary)
+bcu_inflow = DirichletBC(V, Constant((uin, 0.0)), inflowBoundary)
+# bcu_walls = DirichletBC(V, Constant((0.0, 0.0)), slipBoundary)
+# bcp_inflow = DirichletBC(Q, Constant(1.0), inflowBoundary)
+bcp_outflow = DirichletBC(Q, Constant(0), outflowBoundary)
+bcu = [bcu_inflow]
+bcp = [bcp_outflow]
+
+# Define trial and test functions
+u = TrialFunction(V)
+v = TestFunction(V)
+p = TrialFunction(Q)
+q = TestFunction(Q)
+
+# Define functions for solutions at previous and current time steps
+u0 = Function(V)
+u1  = Function(V)
+p0 = Function(Q)
+p1  = Function(Q)
+
+# Define expressions used in variational forms
+
+dt = 0.5*mesh.hmin()
+U  = 0.5*(u0+u)
+um = 0.5*(u0+u1)
+n  = FacetNormal(mesh)
+f  = Constant((0, 0))
+k  = Constant(dt)
+mu = Constant(mu)
+rho = Constant(rho)
+
+# Mark boundaries for integration.
+ib = 1
+ob = 2
+sb = 3
+ 
+boundaries = MeshFunction("size_t", mesh, mesh.topology().dim()-1)
+boundaries.set_all(0)
+inflowBoundary.mark(boundaries, ib)
+outflowBoundary.mark(boundaries, ob)
+slipBoundary.mark(boundaries, sb)
+
+ds = Measure("ds", subdomain_data=boundaries)
+
+beta = 1
+h = CellDiameter(mesh)
+gamma = beta/h;
+
+# u_mag = sqrt(dot(u1,u1))
+# d1 = 1.0/sqrt((pow(1.0/dt,2.0) + pow(u_mag/h,2.0)))
+# d2 = h*u_mag
+
+# Define symmetric gradient
+def epsilon(u):
+	return (nabla_grad(u) + nabla_grad(u).T)/2
+	# return sym(nabla_grad(u))
+
+# Define stress tensor
+def sigma(u, p):
+	return 2*mu*epsilon(u) - p*Identity(len(u))
+
+
+## Formulate the variational problem in Incremental pressure correction scheme (IPCS)
+## Good description/derivation here:
+## http://www.diva-portal.org/smash/get/diva2:1242050/FULLTEXT01.pdf
+
+# Define variational problem for step 1 (Tentative velocity solution)
+F1 = rho*dot((u - u0) / k, v)*dx + \
+	rho*dot(dot(u0, nabla_grad(u0)), v)*dx + \
+	inner(sigma(U, p0), epsilon(v))*dx - \
+	dot(f, v)*dx + \
+	dot(p0*n, v)*ds - dot(mu*nabla_grad(U)*n, v)*ds + \
+	gamma*inner(dot(u, n), dot(v, n))*ds(sb) 
+
+# F1 = rho*inner((u-u0)/k, v)*dx + inner(mu*grad(u), grad(v))*dx \
+# 	+ rho*inner(dot(u0,nabla_grad(u0)), v)*dx + inner(nabla_grad(p0), v)*dx \
+# 	+ inner(mu*dot(nabla_grad(u) + nabla_grad(u).T, n) - p0*n, v)*ds(sb) \
+# 	+ inner(f, v)*dx \
+# 	+ gamma*inner(dot(u, n), dot(v, n))*ds(sb)
+
+a1 = lhs(F1)
+L1 = rhs(F1)
+
+# Define variational problem for step 2 (Pressure update)
+a2 = dot(nabla_grad(p), nabla_grad(q))*dx
+L2 = dot(nabla_grad(p0), nabla_grad(q))*dx - (1/k)*div(u1)*q*dx
+
+# Define variational problem for step 3 (Velocity correction)
+a3 = dot(u, v)*dx ##+ gamma*inner(dot(u, n), dot(v, n))*ds(sb)
+L3 = dot(u1, v)*dx - k*dot(nabla_grad(p1 - p0), v)*dx
+
+# Define force measurement
+psiExp = Expression(("near(x[1], 0) || near(x[1], H) ? 0.0 : 1.0", "0.0"), H=ymax, element = V.ufl_element())
+psi = interpolate(psiExp, V)
+force = inner((u1 - u0)/k + grad(um)*um, psi)*ds(sb) - p1*div(psi)*ds(sb) + mu/rho*inner(grad(um), grad(psi))*ds(sb)
+
+
+# Assemble matrices
+# A1 = assemble(a1)
+# A2 = assemble(a2)
+# A3 = assemble(a3)
+
+# Apply boundary conditions to matrices
+# [bc.apply(A1) for bc in bcu]
+# [bc.apply(A2) for bc in bcp]
+
+# Create XDMF files for visualization output
+# xdmffile_u = XDMFFile('navier_stokes_cylinder/{}/{}/velocity.xdmf'.format(angle, aspect_ratio))
+# xdmffile_p = XDMFFile('navier_stokes_cylinder/{}/{}/pressure.xdmf'.format(angle, aspect_ratio))
+xdmffile_u = XDMFFile('navier_stokes_cylinder/velocity.xdmf')
+xdmffile_p = XDMFFile('navier_stokes_cylinder/pressure.xdmf')
+
+# Time-stepping
+t = 0
+time0 = time.time()
+n = 0
+
+t_array = []
+f_array = []
+
+print(int(T/dt), "iterations total")
+while t < T:
+
+	for i in range(10):
+		# Update current time
+		# uinflow.t = t;
+		t += dt
+
+		try:
+			# Step 1: Tentative velocity step
+			A1 = assemble(a1)
+			b1 = assemble(L1)
+			[bc.apply(A1) for bc in bcu]
+			[bc.apply(b1) for bc in bcu]
+			solve(A1, u1.vector(), b1, 'bicgstab', 'hypre_amg')
+
+			# Step 2: Pressure correction step
+			A2 = assemble(a2)
+			b2 = assemble(L2)
+			[bc.apply(A2) for bc in bcp]
+			[bc.apply(b2) for bc in bcp]
+			solve(A2, p1.vector(), b2, 'bicgstab', 'hypre_amg')
+
+			# Step 3: Velocity correction step
+			A3 = assemble(a3)
+			b3 = assemble(L3)
+			[bc.apply(A3) for bc in bcu]
+			[bc.apply(b3) for bc in bcu]
+			solve(A3, u1.vector(), b3, 'cg', 'sor')
+
+			# Update previous solution
+			u0.assign(u1)
+			p0.assign(p1)
+
+		except RuntimeError:
+			plt.figure()
+			plot(mesh)
+			plot(p0)
+			plot(u0)
+			plt.show()
+			plt.close()
+
+			print("Divergent solution")
+
+			raise
+
+		n+=1
+
+	t_array.append(t)
+	f_array.append(assemble(force))
+
+	# Save solution to file (XDMF/HDF5)
+	xdmffile_u.write(u1, t)
+	xdmffile_p.write(p1, t)
+
+	# Update progress bar
+	timeleft = (time.time()-time0)*(1-t/T)/(t/T)
+	# sys.err.write(str(int(t / T*100)) + "%" + str(round(timeleft,1))+ "s    \r");
+	print(int(t / T*100), "%", round(timeleft,1), "s", end="   \r", file=sys.stderr)
+	
+# print()
+print(str(round(time.time()-time0,1)) + "s")
+print("Drag:", np.average(f_array[len(f_array)//2:]), np.std(f_array[len(f_array)//2:]))
+
+plt.figure(figsize=(8,6))
+plot(u1)
+plt.show()
+plt.savefig(str(aspect_ratio)+":"+str(angle)+".png")
+plt.close()
+
---
+++ Projects/mesh.py
@@ -0,0 +1,124 @@
+from fenics import *
+import mshr
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def testMesh(res=32, segments=32):
+	l = 2.2
+	h = 0.41;
+	domain = mshr.Rectangle(Point(0,0), Point(l,h))
+	obj = mshr.Circle(Point(0.3, 0.2), 0.05, res);
+
+	mesh = mshr.generate_mesh(domain-obj, res)
+	# mesh = refineMesh(mesh, 0.2, 0.8, 0.05, 0.36);
+
+	return mesh, (l,h)
+
+def wedge(width, radius, angle, segments):
+	a = width/2-radius
+	b = a*np.tan(angle)
+	c = np.sqrt(a**2+b**2)
+
+	w1 = roundRect(radius, c+2*radius, segments)
+	w2 = roundRect(radius, c+2*radius, segments)
+
+	if(radius < 1/4 and angle > 0):
+		w1 += mshr.Rectangle(Point(radius,radius), Point(c, -c+radius))
+		w2 += mshr.Rectangle(Point(radius,radius), Point(c, c-radius)) 
+
+	w1 = mshr.CSGRotation(w1, Point(radius, radius), np.pi/2-angle)
+	w2 = mshr.CSGRotation(w2, Point(radius, radius), -np.pi/2+angle)
+
+	w3 = roundRect(radius, width, segments)
+	w3 = mshr.CSGRotation(w3, Point(radius, radius), np.pi/2)
+	w3 = mshr.CSGTranslation(w3, Point(b, 0))
+
+	w5 = mshr.Rectangle(Point(b+2*radius,0), Point(b+width, width))
+
+	return mshr.CSGTranslation(w1+w2, Point(0,width/2-radius)) + w3 - w5
+
+
+def roundRect(radius, length, segments):
+	shape = mshr.Circle(Point(radius, radius), radius, segments)
+	shape += mshr.Rectangle(Point(radius,0), Point(length-radius, radius*2))
+	shape += mshr.Circle(Point(length-radius, radius), radius, segments)
+
+	return shape;
+
+
+def train(angle, length, width, radius=1/10, segments=20):
+	body = mshr.Rectangle(Point(0,0), Point(length, width))
+	front = wedge(width, radius, np.pi*angle/180, segments);
+	back = mshr.CSGRotation(front, Point(0, width/2), np.pi)
+
+	delta = (width/2-radius)*np.tan(np.pi*angle/180)+radius;
+	front = mshr.CSGTranslation(front, Point(-delta, 0))
+	back = mshr.CSGTranslation(back, Point(length+delta,0))
+
+	return front + body + back;
+
+
+def tunnel(length, height):
+	return mshr.Rectangle(Point(0,0), Point(length, height))
+
+
+def refineMesh(mesh, x0, x1, y0, y1):
+	cell_marker = MeshFunction("bool", mesh, mesh.topology().dim())
+	for cell in cells(mesh):
+		cell_marker[cell] = False
+		p = cell.midpoint()
+		if x0 <= p.x() <= x1 and y0 <= p.y() <= y1:
+			cell_marker[cell] = True
+	return refine(mesh, cell_marker)
+
+
+def createMesh(length, resolution, angle, aspect_ratio, dx, domain, rear=False, full=True, double=False, radius=1/10, segments=24):
+	H = 1 + aspect_ratio;
+	L = domain
+
+	if(double):
+		H*=2;
+	if(full):
+		L = length + 3*dx
+
+	domain = tunnel(L, H);
+	obj = train(angle, length, 1.0, radius=radius, segments=segments)
+	if(rear):
+		obj = mshr.CSGTranslation(obj, Point(0, aspect_ratio/2))
+	else: 
+		obj = mshr.CSGTranslation(obj, Point(dx, aspect_ratio/2))
+
+	mesh = mshr.generate_mesh(domain-obj, resolution)
+	return mesh, (L, H);
+
+
+
+if __name__ == "__main__":
+
+	mesh, (l,h) = createMesh(3, 16, 30, 1/5, 2, 3, full=False)
+	mesh = refineMesh(mesh, 1.5, l, 0, h)
+	plt.figure()
+	plot(mesh)
+	plt.show()
+
+	# angle = 0
+	# t = wedge(1, 1/5, np.pi*angle/180, 20)
+	# mesh = mshr.generate_mesh(t, 16)
+
+	# plt.figure()
+	# plot(mesh)
+	# plt.show()
+
+	# t = train(30, 3, 1, radius=1/3)
+
+	# for angle in range(0,45,10):
+	# 	for r in range(3,10):
+	# 		print(angle, r)
+	# 		t = wedge(1, 1/r, np.pi*angle/180, 20)
+	# 		mesh = mshr.generate_mesh(t, 16)
+
+	# 		plt.figure()
+	# 		plot(mesh)
+	# 		plt.savefig(str(angle)+":"+str(r)+".png")
+	# 		plt.close()
